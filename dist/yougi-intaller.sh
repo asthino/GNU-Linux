fc4875e72a06acb3042596fc7a6c650bbaa72251
azafiamy <azafiamy@infodev.ovh>
2024-01-05 11:13:57 +0100

    [E] - check commits script

--- dist/yougi-intaller.sh
+++ dist/yougi-intaller.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 REPO="https://github.com/asthino/GNU-Linux.git"
 
-cd "/usr/local/bin/"
+cd "/usr/bin/"
 if command -v git &> /dev/null; then
-    git clone yougit $REPO
+    git clone $REPO yougit
     cd yougit
 
     sudo chmod +x ./yougit.sh
-    echo "alias yougit='/usr/local/bin/yougit/yougit.sh'" >> ~/.bashrc
+    echo "alias yougit='/usr/bin/yougit/yougit.sh'" >> ~/.bashrc
     source ~/.bashrc
 else
     echo "Git n'est pas installé sur ce système."
--- yougit.sh
+++ yougit.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-DIR="./"
+if "$1" -eq ""; then
+    DIR="./"
+else 
+    DIR="$1"
+fi
 
+# Vérifi si git est installé
 if command -v git &> /dev/null; then
     cd $DIR
+    # Vérifie si le répertoire actuel est un repo git
     if git rev-parse --is-inside-work-tree &> /dev/null; then
-        git diff
+        while true; do
+        # Vérifie s'il y a plus des modifications non commitées dans le dépôt Git
+        count=$(git status --porcelain | wc -l)
+        threshold=2
 
-        git status
+        if [ "$count" -gt "$threshold" ]; then
+            echo -e "\e[1;31mAttention ! Il y a plus de 2 modifications non commitées dans votre dépôt Git.\e[0m"
+        fi
+
+        # Attente de 5 secondes avant la prochaine vérification
+        sleep 5
+        done &
     else
         echo "Vous n'êtes pas dans un répertoire Git."
     fi
